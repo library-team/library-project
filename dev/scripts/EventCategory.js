cf6637cdb5902d96c037dc78bdbaa53bd2f78f82
Alan Jones <jones.alan21@gmail.com>
2018-05-23 17:16:36 -0400

    created component files, added firebase

---
+++ dev/scripts/EventCategory.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EventCategory = (props) => {
+    return (
+        <div>hello</div>
+    )
+}
+
+export default EventCategory;
---
+++ dev/scripts/EventPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EventPage = (props) => {
+    return (
+        <div>hello</div>
+    )
+}
+
+export default EventPage;
