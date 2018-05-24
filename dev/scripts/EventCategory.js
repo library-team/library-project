facf404f7a6d530e18ff5519ae39f0387a2feb27
Alan Jones <jones.alan21@gmail.com>
2018-05-24 17:47:50 -0400

    added styling on event categories and upcoming events

--- dev/scripts/EventCategory.js
+++ dev/scripts/EventCategory.js
@@ -39,7 +39,7 @@ class EventCategory extends React.Component {
     render() {
 
         return (
-        <section>
+        <section className="EventCategory">
             <h2>{this.state.title}</h2>
             {this.props.appState.slice(0,this.state.loadNumber).map((event, i) => {
                 
@@ -47,10 +47,17 @@ class EventCategory extends React.Component {
                 <div className="event">
                     <h3>{event.title}</h3>
                     <h4>{event.date}</h4>
+                        <p dangerouslySetInnerHTML={{ __html: event.description }}></p>
+                    <ul>
+                        <li><a href="#"><i class="fas fa-info-circle"></i> Event Info</a></li>
+                        <li><a href="#"><i class="fas fa-bookmark"></i> Save Event</a></li>
+                        <li><a href="#"><i class="fas fa-calendar-plus"></i> Add to Calendar</a></li>
+                    </ul>
+                    
                 </div>
                 )
             })}
-            <a href="#" onClick={this.loadMore}>See more...</a>
+            <a className="button" href="#" onClick={this.loadMore}>See more events...</a>
         </section> 
         )
     }
